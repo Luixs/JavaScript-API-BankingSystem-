842076135ed83b5492c94cc0746355c2b3ae39d0
Luis Starlino <luis.guilherme009@gmail.com>
2021-05-06 21:39:40 -0300

    Finished the main route and started the create others routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,14 @@
 const { Router } = require ('express');
 /* IMPORTAR CONTROLLERS */
 
-const PokemonController = require('../controllers/PokemonController');
-const PokeTipoController = require('../controllers/PokeTipoController');
+//const PokemonController = require('../controllers/PokemonController');
+//const PokeTipoController = require('../controllers/PokeTipoController');
 
 const routes = Router();
 
-routes.get('/', (req,res){
-    res.status(200).json({Mensagem: "Hello, welcome to the Bank System!!"})
+routes.get('/', (req,res)=>{
+    res.status(200).json({mensagem: "Hello, Welcome to the Bank System"});
 })
+
 
 module.exports = routes;
