57798d5ddbad891b42d44131d93c09c5ea2226c4
Luis Starlino <luis.guilherme009@gmail.com>
2021-05-06 21:06:17 -0300

    Finished the migrations

--- migrations/20210506233438-create-agency.js
+++ migrations/20210506233438-create-agency.js
@@ -34,7 +34,6 @@ module.exports = {
           allowNull: false,
           type: Sequelize.DATE
         }    
-
       }
      })
     /**
--- migrations/20210506233500-create-clients.js
+++ migrations/20210506233500-create-clients.js
@@ -2,15 +2,41 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('clients', {
        id:{
         primaryKey: true,
         allowNull: false,
         autoIncrement: true,         
         type: Sequelize.INTEGER
        },
-       
-
+       name:{
+         allowNull: false,
+         type: Sequelize.STRING,
+       },
+       cpf: {
+         allowNull: false,
+         type: Sequelize.INTEGER
+       },
+       gender: {
+         allowNull: false,
+         type: Sequelize.STRING
+       },
+       wage: {
+         allowNull: false,
+         type: Sequelize.INTEGER
+       },
+       contact: {
+        allowNull: false,
+        type: Sequelize.STRING
+       },
+       createdAt:{
+         allowNull: false,
+         type: Sequelize.DATE          
+       },
+       updatedAt:{
+         allowNull: false,
+         type: Sequelize.DATE
+       }    
       })
     /**
      * Add altering commands here.
@@ -21,6 +47,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('clients');
     /**
      * Add reverting commands here.
      *
