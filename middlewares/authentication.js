cab4c94ff162a025c91758dc6fe4a107f094862e
Luis Starlino <luis.guilherme009@gmail.com>
2021-05-27 10:58:04 -0300

    Using the Bearer token auth

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -2,7 +2,11 @@
 const jwt = require('jsonwebtoken');
 
 let verificar = function verificationJwt(req,res,next){
-    const token = req.headers['x-acess-token'];
+
+    /* ---- BEARER VERIFICATION --- */
+    let token = req.headers['authorization'];
+    const newToken = token.split(" ");
+    token = newToken[1];
     
     if(!token) res.status(401).json({message:"User not Authorizaded!!"})//DEFINIR MENSAGEM
 
