105fa9a96fb42b0d173a862124a5d41855aae278
Luis Starlino <luis.guilherme009@gmail.com>
2021-06-16 09:02:51 -0300

    Making a  login usinga User from the Mysql, database

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,38 +25,21 @@ class autoController{
             }
             //INVALID PASSOWORD
             else if (!await bcrypt.compare(password, findUser.password)){
-                return res.status(401).json({Message:"Invalid Password!!!"});
+                return res.status(401).json({Message:"User or Password is Invalid!!!"});
             }
             else{
                 let role = await isAdmin(findUser);
                 console.log(role);
+                const token = jwt.sign({id: findUser.id, role: role}, process.env.ACCESS_SECRET,{
+                    expiresIn: 300 //expire in 5min
+                });
+                res.status(200).json({
+                    auth: true,
+                    message: `Login sucessful, welcome ${findUser.name}`,
+                    token: token
+                })
             }
-            //SIMPLE USER (Modify to get a User from the SQL)
-            /*if(req.body.user == "luixs" && req.body.password == "123"){
-                let id = 44;
-                let role = "user";
-                const token = jwt.sign({id: id, role: role}, process.env.ACCESS_SECRET,{
-                    expiresIn: 300 //expire in 5Min
-                });
-                res.status(200).json({
-                    auth: true,
-                    token: token
-                })
-            }//ADMIN USER (Modify to get a ADMIN from the SQL)
-            else if(req.body.user == "admin" && req.body.password == "admin"){
-                let id = 88;
-                let role="admin";
-                const token = jwt.sign({id: id, role: role}, process.env.ACCESS_SECRET,{
-                    expiresIn: 300 //expire in 5Min
-                });
-                res.status(200).json({
-                    auth: true,
-                    token: token
-                })
-            }else{
-                res.status(401).json({message: "User not authorized, try a new one"})
-            }*/
-
+            
         } catch (error) {
             res.status(400).json({erro: error.message});
         }
